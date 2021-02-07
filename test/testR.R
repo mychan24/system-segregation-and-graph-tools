7eb4c7c0522393d1dae8c62b22374fe6502b2e6a
Micaela Chan <4413270+mychan24@users.noreply.github.com>
2021-02-07 14:51:40 -0500

    Clean up unit test for segregation/segregation_type R script.
    * ./test/testR.R

--- test/testR.R
+++ test/testR.R
@@ -3,61 +3,47 @@
 ## Source scripts
 source("./R_scripts/segregation.R")
 source("./R_scripts/segregation_by_type.R")
+load('./test/testanswer.RData')
 
-# Load test data
-# m <- as.matrix(read.table("./test/testmat1.txt",header=F, sep=","))
-# ci <- read.table("./test/ci1.txt",header=F)$V1
-
-## Define correct answer
-coranswer <- list()
-coranswer$W <- 6
-coranswer$B <- 2
-coranswer$S <- 0.625
-
-m <- matrix(NA,4,4)
+#### 1. Test segregation() ####
+m <- matrix(NA,4,4)   # Create test data
 m[1,] <- c(1,4,2,2)
 m[2,] <- c(4,1,3,2)
 m[3,] <- c(2,3,1,8)
 m[4,] <- c(2,2,8,1)
 
-ci <- c(1,1,2,2)
+ci <- c(1,1,2,2)      # Test community assignment
 
-
-##### 1. Test segregation() ####
 out <- segregation(M = m, Ci = ci,diagzero = T, negzero = T)
 
-if(!identical(out, coranswer)){
-  print("ERROR! segregation() returned incorrect result. Check source code")
+if(!identical(out, seg_c)){
+  print("[2] ERROR! segregation() returned incorrect result. Check source code")
 }else{
-  print("segregation() returned the correct test results.")
+  print("[1] PASSED: segregation() returned the correct test results.")
 }
 
-# #### 2. Test segregation_by_type() ####
-coranswer <- list()
-coranswer$W <- 6
-coranswer$B <- 2
-coranswer$S <- 0.625
-
+#### 2. Test segregation_by_type() ####
 ci <- c(1,1,2,2,3,3,4,4)
-c_type <- 
+c_type <- c(3,3,3,3,2,2,2,2) # make Ci in an order thta has be sorted
 
 m <- matrix(NA,8,8)
-m[1,] <- c(1,4,2,2,4,4,2,2)
-m[2,] <- c(4,1,3,2,2,2,4,4)
-m[3,] <- c(2,3,1,8,1,4,2,2)
-m[4,] <- c(2,2,8,1,4,2,2,2)
-m[5,] <- c(4,2,1,4,1,4,2,1)
-m[6,] <- c(4,2,4,2,4,1,1,1)
-m[7,] <- c(2,4,2,2,2,1,1,2)
-m[8,] <- c(2,4,2,2,1,1,2,1)
+m[1,] <- c(1,0,0,0,1,1,0,1)
+m[2,] <- c(0,1,2,2,2,2,0,2)
+m[3,] <- c(0,2,1,4,1,4,2,2)
+m[4,] <- c(0,2,4,1,0,2,2,2)
+m[5,] <- c(1,2,1,0,1,4,0,0)
+m[6,] <- c(1,2,4,2,4,1,1,1)
+m[7,] <- c(0,0,2,2,0,1,1,0)
+m[8,] <- c(1,2,2,2,0,1,0,1)
 
 
-segregation_by_type(M = m, Ci = c, C_Type = , diagzero = T, negzero = T)
+out_type <- segregation_by_type(M = m, Ci = ci, C_Type = c_type, diagzero = T, negzero = T)
 
-
-
-
-
+if(!identical(out_type, seg_type_c)){
+  print("[2] ERROR! segregation_type() returned incorrect result. Check source code")
+}else{
+  print("[2] PASSED: segregation_type() returned the correct test results.")
+}
 
 
 
